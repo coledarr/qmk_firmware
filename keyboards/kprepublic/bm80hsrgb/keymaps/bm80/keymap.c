2f3c8ccd4b61fddb18885ab76c4050cc6e6a213e
Darren Cole <virr@virr.net>
2022-09-03 22:26:14 -0700

    Startup in preferred RGB mode, highlight layer keys and layer streaming keys

--- keyboards/kprepublic/bm80hsrgb/keymaps/bm80/keymap.c
+++ keyboards/kprepublic/bm80hsrgb/keymaps/bm80/keymap.c
@@ -58,3 +58,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+void keyboard_post_init_user(void){
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_REACTIVE);
+}
+
+// function key for now
+#define INDICATOR_KEY 82
+#define INDICATOR_COLOR RGB_RED
+#define STREAM_LAYER 2
+#define STREAM_KEYS_COLOR RGB_GOLD
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+        uint8_t streamingKeys[] = {1, 4, 5, 8};
+        if (STREAM_LAYER == layer){
+            for(uint8_t i=0;i < sizeof(streamingKeys) / sizeof(streamingKeys[0]); i++){
+                rgb_matrix_set_color(streamingKeys[i], STREAM_KEYS_COLOR);
+            }
+            rgb_matrix_set_color(INDICATOR_KEY, INDICATOR_COLOR);
+        }else{
+            for(uint8_t row=0; row < MATRIX_ROWS; ++row){
+                for(uint8_t col=0; col <MATRIX_COLS; ++col){
+                    uint8_t index = g_led_config.matrix_co[row][col];
+
+                    if (index >= led_min
+                            && index <= led_max
+                            && index != NO_LED
+                            && keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                        rgb_matrix_set_color(index, INDICATOR_COLOR);
+                    }
+                }
+            }
+        }
+    }
+}
